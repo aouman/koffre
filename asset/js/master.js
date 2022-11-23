cca93266672036984a99cc678f7efaacc9668e99
aouman <k2.aouman@gmail.com>
2022-11-23 21:53:39 +0000

    Creation des tabs pour l'affichage des projets

--- asset/js/master.js
+++ asset/js/master.js
@@ -1,31 +1,17 @@
 $(function(){
     let current = location.pathname;
-    $('.menu-items ul li').each(function(){
-        let $this = $(this);
-        // if the current path is like this link, make it active
-        if($this.attr('href').indexOf(current) !== -1){
-            $this.addClass('act-show');
-        }
+      $('.menu-items ul li').each(function(){
+          let $this = $(this);
+          // if the current path is like this link, make it active
+          if($this.attr('href').indexOf(current) !== -1){
+              $this.addClass('act-show');
+      }
     });
-
-
-
-
   });
 
-
-  // Show the first tab and hide the rest
-$('.tab-menus li:first-child').addClass('active_tab');
-$('.content').hide();
-$('.content:first').show();
-
-// Click function
-$('.tab-menus li').click(function(){
-  $('.tab-menus li').removeClass('active_tab');
-  $(this).addClass('active_tab');
-  $('.content').hide();
-
-  let activeTab = $(this).find('a').attr('href');
-  $(activeTab).fadeIn();
-  return false;
+$(".Click-here").on('click', function () {
+  $(".custom-model-main").addClass('model-open');
+});
+$(".close-btn, .bg-overlay").click(function () {
+  $(".custom-model-main").removeClass('model-open');
 });
