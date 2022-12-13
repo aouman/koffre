53cfbc45cd118d68c31cfe0cfce4ef201a548c92
aouman <k2.aouman@gmail.com>
2022-12-13 08:17:52 +0000

    creation des page des taches telque mes équipes

--- asset/js/master.js
+++ asset/js/master.js
@@ -8,10 +8,45 @@ $(function(){
       }
     });
   });
-
-$(".Click-here").on('click', function () {
+$(".add_project").on('click', function () {
   $(".custom-model-main").addClass('model-open');
 });
-$(".close-btn, .bg-overlay").click(function () {
+$(".close-btn").click(function () {
   $(".custom-model-main").removeClass('model-open');
+});
+
+$(".add_team56").on('click', function () {
+  $(".adds_teams").addClass('model-open');
+});
+$(".close-btn").click(function () {
+  $(".adds_teams").removeClass('model-open');
+});
+
+$(".show_team").on('click', function () {
+  $(".showTeam").addClass('model-open');
+});
+$(".close-btn").click(function () {
+  $(".showTeam").removeClass('model-open');
+});
+
+$(".card_team_member").on('click', function () {
+  $(".showMberTeam").addClass('model-open');
+});
+$(".close-btn").click(function () {
+  $(".showMberTeam").removeClass('model-open');
+});
+
+$(".update_team").on('click', function () {
+  $(".updateTeams").addClass('model-open');
+  $(".showTeam").removeClass('model-open');
+});
+$(".close-btn").click(function () {
+  $(".updateTeams").removeClass('model-open');
+});
+
+$(".card_proj-task").on('click', function () {
+  $(".showsTasks").addClass('model-open');
+});
+$(".close-btn").click(function () {
+  $(".showsTasks").removeClass('model-open');
 });
